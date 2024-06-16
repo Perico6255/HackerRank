7c08bd8f7fbd8bb4df14bcd7fef545805416a40e
Perico6255 <ruizcandaupedro@gmail.com>
2024-06-16 18:03:34 +0200

    HourGlassSum fix the bug for all negative case

--- hourGlassSum/hourGlassSum.go
+++ hourGlassSum/hourGlassSum.go
@@ -10,7 +10,7 @@ func HourGlassSum(arr [][]int32)int32{
     for i := 0; i < 4; i++ {
       now = arr[j][i] + arr[j][i+1] + arr[j][i+2] + arr[j+1][i+1] + arr[j+2][i] + arr[j+2][i+1] + arr[j+2][i+2]
       fmt.Println(now)
-      if max < now {
+      if max < now || (i==0 && j==0)  {
         max = now
       }
     }
